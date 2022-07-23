10d247a928ba3983832222880a081f5ab9be3bf0
OSHMOS <87176797+OSHMOS@users.noreply.github.com>
2022-07-24 00:21:57 +0900

    쿼리 매개변수 기본값

--- main.py
+++ main.py
@@ -3,23 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def hello():
-    return "Hello, World!"
-
-
-@app.get("/users/me")
-def get_current_user():
-    return {"user_id": 123}
-
-
-# no type hint
-# @app.get("/users/{user_id}")
-# def get_user(user_id):
-#     return {"user_id": user_id}
-
-
-# type hint
-@app.get("/users/{user_id}")
-def get_user(user_id: int):
-    return {"user_id": user_id}
+@app.get("/users")
+def get_users(limit: int = 100):
+    return {"limit": limit}
